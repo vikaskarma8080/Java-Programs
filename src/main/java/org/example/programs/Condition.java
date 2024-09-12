c9828204b74b17ff00cb6b1625faa42cd9961c6e
vikaskarma8080 <vikaskarma8080@gmail.com>
2024-09-12 18:10:09 +0530

    Java program

---
+++ src/main/java/org/example/programs/Condition.java
@@ -0,0 +1,11 @@
+package org.example.programs;
+
+public class Condition {
+    public static void main(String[] args) {
+        boolean learning =true;
+        if(learning)
+            System.out.println("Java developer");
+        else
+            System.out.println("What are you doing there..");
+    }
+}
---
+++ src/main/java/org/example/programs/IfElse.java
@@ -0,0 +1,20 @@
+package org.example.programs;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int passingMarks,marksObtained;
+        passingMarks=40;
+        Scanner sc=new Scanner(System.in);
+        marksObtained =sc.nextInt();
+        if(marksObtained>=passingMarks)
+        {
+            System.out.println("passed");
+        }
+        else
+        {
+            System.out.println("failed");
+        }
+    }
+}
