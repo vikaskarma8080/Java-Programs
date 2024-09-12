4859951e1688145368f8c3cb85c10c8112f47cf9
vikaskarma8080 <vikaskarma8080@gmail.com>
2024-09-12 14:58:23 +0530

    Java program

---
+++ src/main/java/org/example/programs/PrintInteger.java
@@ -0,0 +1,9 @@
+package org.example.programs;
+
+public class PrintInteger {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++) {
+            System.out.println(i);
+        }
+    }
+}
