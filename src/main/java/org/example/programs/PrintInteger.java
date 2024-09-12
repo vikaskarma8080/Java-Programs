61a0f8a2582512f02d4db02e00b1cb31e6e83bed
vikaskarma8080 <vikaskarma8080@gmail.com>
2024-09-12 16:50:03 +0530

    Java program

--- src/main/java/org/example/programs/PrintInteger.java
+++ src/main/java/org/example/programs/PrintInteger.java
@@ -2,7 +2,7 @@ package org.example.programs;
 
 public class PrintInteger {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++) {
+        for(int i=0;i<=10;i++) {
             System.out.println(i);
         }
     }
